4dbe587552ba474e75c1ddedbd8d8cfa51616991
João Moura <79586002+JohnnyMinho@users.noreply.github.com>
2023-06-19 16:42:16 +0100

    Add files via upload

--- LZWd.C
+++ LZWd.C
@@ -45,14 +45,20 @@ trie_node* criar_node(char data){
     }
     return novo_node;
 }
+/*
 
-unsigned char* partir_blocos(){
-
+char* partir_blocos(int tamanho_bloco_txt, int tamanho_lido, FILE* file){
+    char* bloco_text = (char*)malloc(tamanho_bloco_txt*sizeof(char));
+    fseek(file,tamanho_lido,SEEK_SET);
+    fread(bloco_text,tamanho_bloco_txt,sizeof(char),file);
+    printf("bloco lido %s", &bloco_text);
+    return bloco_text;
 }
 
-unsigned char* unir_blocos(){
+char* unir_blocos(){
 
 }
+*/
 
 void mostrar_est(){ 
 
@@ -69,7 +75,7 @@ void print_time(){
 
     strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", local_time);
 
-    printf(": %s\n", time_string);
+    printf("Tempo do início da execução: %s\n", time_string);
 }
 /*bool reached_eof(char* str){
     for(int i = 0; str[i] != '\0'; i++){
@@ -95,8 +101,11 @@ void libertar_nodes(trie_node* node, int n_filhos){
 trie_node* inserir_na_trie(trie_node* root, char* padrao, int n_filhos){
     //Como vamos inserir no dicionário, nós têmos de percorrer o dicionário da maneira mais eficiente
     //Basicamente ao percorrer têmos de tentar encontrar a raiz do mesmo
+    //Podemos guardar o prefixo atual e no fim fazemos uma procura a partir da posição atual no bloco de texto até deixarmos de encontrar prefixos válidos ex:
+    //AABBAB -> Este ao fim de 5 iterações pode usar AB como um prefixo
     trie_node* temp = root;
-    for(int i = 0; padrao[i] != '/0'; i++){
+    
+    for(int i = 0; padrao[i] != NULL; i++){
         int index_ascii = (int) padrao[i];
     //for(int i = 0; i<n_filhos; i++){
         //Procura pelo index do prefixo do padrao
@@ -118,8 +127,11 @@ int procurar_na_trie(trie_node* root, char* padrao){
 
 
 int main(int argc, char *argv[]) {
+    int dicionario_base = 1; //Dicionário é o báscio (Tabela ASCII), se não for ativada a opção de um dicionário custom | 0 -> Dicionário Custom | 1 -> Dicionário Base 
+    trie_node* raiz = criar_node('\0');
     int fd;
     int dicionario_default = 0;
+    int position_in_file = 0;
     //bool end_file = false;
     int nread = 0;
     int block_size;
@@ -143,27 +155,30 @@ int main(int argc, char *argv[]) {
         block_size = basic_block_size;
     }
 
-
-
-
-    //Fim impressões Iniciais
-    char file_content_block[basic_block_size];
-    fd = open(argv[1], O_RDONLY, 0666);
+    
+    FILE* file = fopen(argv[1], "r");
     if(fd == -1){
         printf("O caminho até ao ficheiro está errado ou o ficheiro não existe");
         return 1;
     }
     struct stat st;
     fstat(fd,&st);
-
+    
     //Impressões Iniciais
-    printf("Autoria:%s", &st.st_uid);
+    printf("Autoria:%ld", &st.st_uid); //Impressão numérica do ID do autor do ficheiro
     print_time();
     printf("Nome do ficheiro de entrada, %s \n",argv[1]);
     printf("Nome do ficheiro de saída, %s \n",argv[2]);
-    while(nread = read(fd, file_content_block, sizeof(file_content_block)) > 0){
-        
+    
+    //Fim impressões iniciais
+    //Têmos de ter uma maneira de introduzir um dicionário
+    while(!feof(file)){
+        char* text_block = (char*)malloc(sizeof(char)*block_size);
+        fseek(file,position_in_file,SEEK_SET);
+        position_in_file += block_size;
+        fread(text_block,sizeof(char), block_size,file);
+        inserir_na_trie(raiz,text_block,256);
     }
-    //
+  
     return 0;
 }
